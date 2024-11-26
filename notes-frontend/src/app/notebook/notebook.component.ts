9b1c1d8f543514c939a3a489339c349d88f46ce2
hu <yuandong.hu@partner.bmw.de>
2024-11-26 09:42:11 +0100

    notebook component

--- notes-frontend/src/app/notebook/notebook.component.ts
+++ notes-frontend/src/app/notebook/notebook.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { PrimeNGModule } from '../primeng.module';
 import { TranslocoModule } from '@jsverse/transloco';
 import { CommonModule } from '@angular/common';
@@ -14,8 +14,9 @@ import { CommonModule } from '@angular/common';
 export class NotebookComponent implements OnInit{
   private fb = inject(FormBuilder);
   public form: FormGroup = this.fb.group({
-    id: null as unknown as number, 
-    name: null as unknown as string
+    id: new FormControl(''), 
+    name: new FormControl(''),
+    description: new FormControl('')
   });
   ngOnInit(): void {
   }
